589dd97f7116f40e219cc305813fadf9fe5c992a
josephwjcho93@gmail.com <josephcho@Josephs-MacBook-Pro.local>
2024-06-03 15:57:49 -0400

    finished

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+const shoes = [
+  { name: "Birkenstocks", price: 50, type: "sandal" },
+  { name: "Air Jordans", price: 500, type: "sneaker" },
+  { name: "Air Mahomeses", price: 501, type: "sneaker" },
+  { name: "Utility Boots", price: 20, type: "boot" },
+  { name: "Velcro Sandals", price: 15, type: "sandal" },
+  { name: "Jet Boots", price: 1000, type: "boot" },
+  { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+
+
+app.get('/greetings/:username', (req, res) => {
+    const username = req.params.username;
+    const greetingMessage = `Hello there, ${username}! What a delight it is to see you once more, ${username}.`;
+    res.send(greetingMessage);
+});
+
+app.get('/roll/:number', (req, res) => {
+    const number = req.params.number;
+    
+    if (isNaN(number)) {
+      res.send('You must specify a number.');
+    } else {
+      const max = parseInt(number, 10);
+      const randomRoll = Math.floor(Math.random() * (max + 1));
+      res.send(`You rolled a ${randomRoll}.`);
+    }
+});
+
+app.get('/collectibles/:index', (req, res) => {
+    const index = parseInt(req.params.index, 10);
+    
+    if (isNaN(index) || index < 0 || index >= collectibles.length) {
+      res.send("This item is not yet in stock. Check back soon!");
+    } else {
+      const item = collectibles[index];
+      res.send(`So, you want the ${item.name}? For ${item.price}, it can be yours!`);
+    }
+  });
+
+app.get('/shoes', (req, res) => {
+  let filteredShoes = shoes;
+
+  if (req.query['min-price']) {
+      const minPrice = parseFloat(req.query['min-price']);
+      filteredShoes = filteredShoes.filter(shoe => shoe.price >= minPrice);
+  }
+
+  if (req.query['max-price']) {
+      const maxPrice = parseFloat(req.query['max-price']);
+      filteredShoes = filteredShoes.filter(shoe => shoe.price <= maxPrice);
+  }
+
+  if (req.query.type) {
+      const type = req.query.type.toLowerCase();
+      filteredShoes = filteredShoes.filter(shoe => shoe.type.toLowerCase() === type);
+  }
+
+  res.json(filteredShoes);
+});
+
+
+app.listen(3000, () => {
+    console.log(`Listening on port 3000`);
+});
